abcbb4c7d72e062cf9a0ae33985f9c5bff721ead
Harshks2406 <harshsingh.hks@gmail.com>
2024-08-25 00:56:19 +0530

    Log removed

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -1,7 +1,6 @@
 import { EXPERIENCES } from "../constants/index.js"
 
 const Experience = () => {
-    console.log(EXPERIENCES)
   return (
     <div className="border-b border-neutral-900 pb-4">
     <h1 className="my-20 text-center text-4xl">Experience</h1>

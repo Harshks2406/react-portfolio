348d7cb270db607777c9ba8801ccd6a79ef96c4b
Harshks2406 <harshsingh.hks@gmail.com>
2024-09-03 00:21:48 +0530

    Contacts page margin added

--- src/components/contacts.jsx
+++ src/components/contacts.jsx
@@ -8,7 +8,7 @@ import { MdEmail } from "react-icons/md";
 const Contacts = () => {
   return (
     <motion.div whileInView={{opacity: 1, y: 0}} initial={{opacity: 0, y: -100}} transition={{duration: 0.8}} viewport={{ once: true }} className="border-b border-neutral-900 pb-4">
-        <h1 className="my-10 text-center text-4xl">Get in Touch</h1>
+        <h1 className="my-20 text-center text-4xl">Get in Touch</h1>
         <div className="text-center tracking-tighter">
             <h2>Discuss a project or just want to say hi? My Inbox is open for all. You can reach me on the below links.</h2>
             <h2 className="text-neutral-400">Please do checkout my photography porfolio <a className="border-b"  target="_blank" href="http://ec2-3-6-254-20.ap-south-1.compute.amazonaws.com:2412/">here</a></h2>
